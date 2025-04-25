b71d40073ede55599a3419b17b57246a26035254
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-04-25 12:30:19 -0300

    Banco de Dados
    Essa é a primeira versão do banco de dados, preciso ainda adicionar a empresa, o pedido, a categoria do estoque, o movimento de estoque, e talvez ajustar algumas lógicas.

--- model/db/oblivion_bd.sql
+++ model/db/oblivion_bd.sql
@@ -1,19 +1,66 @@
-CREATE TABLE type_user (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  type_name VARCHAR(10) NOT NULL
+-- Usuário genérico
+CREATE TABLE User (
+  idUser TINYINT AUTO_INCREMENT PRIMARY KEY,
+  nameUser VARCHAR(100) NOT NULL,
+  emailUser VARCHAR(100) NOT NULL UNIQUE,
+  passUser VARCHAR(255) NOT NULL,
+  typeUser BIT NOT NULL
 );
 
--- Inserir papéis padrão
-INSERT INTO roles (type_name) VALUES ('admin'), ('client');
+-- Clientes com dados obrigatórios
+CREATE TABLE Client (
+  idUser TINYINT PRIMARY KEY,
+  cpfClient CHAR(11) NOT NULL UNIQUE,
+  cellClient CHAR(11) NOT NULL UNIQUE,
+  FOREIGN KEY (idUser) REFERENCES User(idUser)
+);
 
--- Tabela de usuários
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  role_id INT,
-  cpf VARCHAR(15), -- Nullable, somente para clientes
-  phone VARCHAR(20), -- Nullable, somente para clientes
-  FOREIGN KEY (role_id) REFERENCES roles(id)
+-- Admins com dados mais simples
+CREATE TABLE Admin (
+  idUser TINYINT PRIMARY KEY,
+  FOREIGN KEY (idUser) REFERENCES User(idUser)
+);
+
+-- Categorias
+CREATE TABLE Category (
+  idCategory TINYINT AUTO_INCREMENT PRIMARY KEY,
+  nameCategory VARCHAR(45) NOT NULL,
+  statusCategory BIT NOT NULL DEFAULT 1
+);
+
+-- Estoque
+CREATE TABLE Stock (
+  idStock SMALLINT AUTO_INCREMENT PRIMARY KEY,
+  quantityStock SMALLINT NOT NULL DEFAULT 0
+);
+
+-- Produtos
+CREATE TABLE Product (
+  idProduct SMALLINT AUTO_INCREMENT PRIMARY KEY,
+  nameProduct VARCHAR(100) NOT NULL,
+  precyProduct DECIMAL(10,2) NOT NULL,
+  codProduct VARCHAR(45) NOT NULL,
+  deliveryProduct BIT NOT NULL DEFAULT 0,
+  statusProduct BIT NOT NULL DEFAULT 1,
+  idCategory TINYINT NOT NULL,
+  idStock SMALLINT NOT NULL,
+  FOREIGN KEY (idCategory) REFERENCES Category(idCategory),
+  FOREIGN KEY (idStock) REFERENCES Stock(idStock)
+);
+
+-- Formas de pagamento
+CREATE TABLE Payment (
+  idPayment TINYINT AUTO_INCREMENT PRIMARY KEY,
+  namePayment VARCHAR(100) NOT NULL,
+  imgPayment VARCHAR(50) NOT NULL,
+  statusPayment BIT NOT NULL DEFAULT 1
+  idConditionPayment TINYINT NOT NULL,
+  FOREIGN KEY (idConditionPayment) REFERENCES Stock(idStock)
+);
+
+-- Condição de pagamento
+CREATE TABLE ConditionPayment (
+  idConditionPayment TINYINT AUTO_INCREMENT PRIMARY KEY,
+  nameConditionPayment VARCHAR(100) NOT NULL,
+  statusPayment BIT NOT NULL DEFAULT 1
 );
