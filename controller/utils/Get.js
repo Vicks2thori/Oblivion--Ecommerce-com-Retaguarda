0ed7828dba78462c87bb49a28c3b516b2451f637
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-05-07 12:10:40 -0300

    Me perdi aqui

---
+++ controller/utils/Get.js
@@ -0,0 +1,34 @@
+//Criando funções comuns para mostrar
+
+const db = require('../../model/db/connection_db.js'); 
+
+//Exibir conforme status
+async function GetStatus(nameTable, status) {
+    let statusTable = "status" + nameTable; //Minhas tabelas todas 
+    const conn = await db.getConnection();
+    try {
+      await conn.query(`
+        SELECT * FROM ${nameTable} WHERE ${statusTable} = ${status}`);
+        console.log('Exibindo produtos por status ', statusProduct);
+    } catch (err) {
+        console.error(`Erro ao exibir ${nameTable}: `, err);
+    } finally {
+        conn.release();
+    }
+}
+
+async function Get(nameTable) {
+    let statusTable = "status" + nameTable; //Minhas tabelas todas 
+    const conn = await db.getConnection();
+    try {
+      await conn.query(`
+        SELECT * FROM ${nameTable}`);
+        console.log('Exibindo produtos por status ', nameTable);
+    } catch (err) {
+        console.error(`Erro ao exibir ${nameTable}: `, err);
+    } finally {
+        conn.release();
+    }
+}
+
+module.export = {GetStatus, Get};
