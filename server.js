37977bda67ef53070fcc56916c95a9c9550e0bd2
Victoria Riso Santana <159472345+Vicks2thori@users.noreply.github.com>
2025-05-07 11:11:49 -0300

    Configuração de servidor

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from 'express'; //importar o modulo do espress
+import morgan from "morgan";
+
 import publicRoutes from "./routes/public.js"; //importação das rotas publicas (E-commerce)
 
 const app = express(); //instanciando
 
 app.use('/', publicRoutes);
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-app.listen(4002); //definido porta base para a "aplicação e escutando" ela
+app.listen(4002, () => {
+    console.log(`Server is listen on: http://localhost:${4002}`);
+}); //definido porta base para a "aplicação e escutando" ela
